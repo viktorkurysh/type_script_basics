3c65957eff45c9383432c06c120eadbc02b90750
Viktor Kurysh <vik8174@gmail.com>
2020-12-05 22:11:13 +0200

    describing classes using interfaces

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { ShoppingCart } from "./lib/shopping-card";
-import { calculateTotalAmount } from './lib/calculate-total-amount';
-import { Order } from "./lib/order";
-
-const cart = new ShoppingCart();
-console.log(`The cart's total is ${calculateTotalAmount(cart)}`);
-
-const order = new Order();
-console.log(`The order's total is ${calculateTotalAmount(order)}`)
